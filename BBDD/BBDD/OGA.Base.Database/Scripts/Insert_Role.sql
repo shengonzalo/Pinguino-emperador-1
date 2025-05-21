0328b70b00ed201f7202170bb74b5c927c0b6bb7
shenhao <108442568+a2158068171@users.noreply.github.com>
2025-05-21 11:40:55 +0200

    Delete BBDD directory

--- BBDD/BBDD/OGA.Base.Database/Scripts/Insert_Role.sql
+++
@@ -1,9 +0,0 @@
-﻿GO
-SET IDENTITY_INSERT [dbo].[Role] ON 
-GO
-IF NOT EXISTS(SELECT 1 FROM [dbo].[Role] WHERE [RoleId] = 1)
-INSERT [dbo].[Role] ([RoleId], [Name], [Description], [Enabled], [IUser], [IDate], [IComments], [UUser], [UDate], [UComments]) 
-VALUES (1, 'ADMINISTRADOR', 'Rol de administrador', 1, '1', GETDATE(), 'Carga inicial', NULL, NULL, NULL)
-GO
-SET IDENTITY_INSERT [dbo].[Role] OFF
-GO
--- BBDD/BBDD/OGA.Base.Database/Scripts/Insert_UserRole.sql
+++
@@ -1,9 +0,0 @@
-﻿GO
-IF NOT EXISTS(SELECT 1 FROM [dbo].[UserRole] WHERE [UserId] = 1 AND [RoleId] = 1)
-INSERT [dbo].[UserRole] ([UserId], [RoleId], [Enabled], [IUser], [IDate], [IComments], [UUser], [UDate], [UComments]) 
-VALUES (1, 1, 1, '1', GETDATE(), 'Carga inicial', NULL, NULL, NULL)
-GO
-IF NOT EXISTS(SELECT 1 FROM [dbo].[UserRole] WHERE [UserId] = 2 AND [RoleId] = 1)
-INSERT [dbo].[UserRole] ([UserId], [RoleId], [Enabled], [IUser], [IDate], [IComments], [UUser], [UDate], [UComments]) 
-VALUES (2, 1, 1, '1', GETDATE(), 'Carga inicial', NULL, NULL, NULL)
-GO
